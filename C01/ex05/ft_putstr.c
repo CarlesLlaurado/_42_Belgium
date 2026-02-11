80b4ef71b789cfffd45cc670c5c7e811e310c01f
CarlesLlaurado <carlesllauradollaurado@gmail.com>
2026-02-11 21:34:28 +0100

    descomentado libreria ex05

--- C01/ex05/ft_putstr.c
+++ C01/ex05/ft_putstr.c
@@ -6,11 +6,12 @@
 /*   By: cllaurad <cllaurad@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 10:20:41 by cllaurad          #+#    #+#             */
-/*   Updated: 2026/02/11 15:34:34 by cllaurad         ###   ########.fr       */
+/*   Updated: 2026/02/11 21:34:03 by cllaurad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 #include <unistd.h>
+/*
 #include <stdio.h>
 */
 void	ft_putstr(char *str)
