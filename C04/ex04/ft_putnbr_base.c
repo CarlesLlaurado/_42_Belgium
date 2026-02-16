38b5e7558b334ca8889fe111f7712059dbe94e0d
CarlesLlaurado <carlesllauradollaurado@gmail.com>
2026-02-16 12:12:00 +0100

    ex04 ft_putnbr_base.c ready to submit

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,49 +6,84 @@
 /*   By: cllaurad <cllaurad@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 12:42:23 by cllaurad          #+#    #+#             */
-/*   Updated: 2026/02/12 13:13:16 by cllaurad         ###   ########.fr       */
+/*   Updated: 2026/02/16 12:11:22 by cllaurad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int number_length(int n)
+int	base_length(char *base)
 {
-    int length = 1;
+	int	length;
 
-    if (n >= 10)
-    {
-        number_length(n / 10);
-        length++;
-    }
-    return length;
+	length = 0;
+	while (base[length] != '\0')
+		length++;
+	return (length);
 }
 
-int base_length(char *base)
+int	valid_base(char *base)
 {
-    int length = 0;
+	int	i;
+	int	j;
+	int	length;
 
-    while (base[length] != '\0')
-        length++;
-    return length;
+	if (!base || base[0] == '\0' || base[1] == '\0')
+		return (0);
+	length = base_length(base);
+	j = 0;
+	i = 0;
+	while (i < length)
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		j = 0;
+		while (j < i)
+		{
+			if (base[i] == base[j])
+			{
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (length);
 }
 
-void intToString(int n, int length, char* num_array)
+void	recursive(unsigned int nbr, unsigned int base_length, char base_ex[])
 {
-    int i = 0;
-    while (i < length)
-    {
-        num_array[length - i] = n % 10;
-        n /= 10;
-        i++;
-    }
+	if (nbr >= base_length)
+	{
+		recursive((nbr / base_length), (unsigned int)base_length, base_ex);
+	}
+	write(1, &base_ex[(nbr % base_length)], 1);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-   int nl = number_length(nbr);
-   int bl = base_length(base);
-   
-   char *num_array[nl];
-   intToString(nbr, nl, *num_array);
+	int				bl;
+	unsigned int	op;
+
+	bl = valid_base(base);
+	if (bl == 0)
+		return ;
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		op = -(unsigned int)nbr;
+	}
+	else
+		op = (unsigned int)nbr;
+	recursive(op, bl, base);
 }
+/*
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        return (0);
+    ft_putnbr_base(atoi(argv[1]), argv[2]);
+}
+*/
